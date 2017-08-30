e9a92ffbd8e106ac9e44d80b4098d511de35faed
EdCollinson1502 <31353239+EdCollinson1502@users.noreply.github.com>
2017-08-30 10:03:05 -0700

    lab 2 day 1

--- Lab 1/lab_2/pair_programming/Harvester.java
+++ Lab 1/lab_2/pair_programming/Harvester.java
@@ -3,9 +3,9 @@ package lab_2.pair_programming;
 
 import kareltherobot.*; 
 /**
- * @author :
- * teacher :
- * due date:
+ * @author : Edward T Collinson 
+ * teacher : Mr. Appel
+ * due date: Tomorrow
  */
 public class Harvester extends Robot
 {
@@ -14,12 +14,87 @@ public class Harvester extends Robot
         super(st, av, dir, numBeepers);
 
     }
-    
-    public void harvest() {
-        //Complete this method, using the methods you create below so that all rows are harvested
+    public void sow() {
+      repositionOrigin();
+      sowTwoRows();
+      sowTwoRows();
+      sowTwoRows();
     }
-    //your methods go below
-    
-    
+    public void harvest() {
+        //Complete this method, using the methods you create below so that all rows are harvested\\
+      move();
+      harvestTwoRows();
+      harvestTwoRows();
+      harvestTwoRows();
+      sow();
+      repositionOrigin();
+    }
+    /**
+     * Precondition: Robot facing east and one block away from a row beepers
+     * Postcondition: Robot facing east, two streets away from where it started, one block away from a beeper
+     */
+    public void harvestTwoRows() {
+        harvestOneRow();
+        repositionLeft();
+        harvestOneRow();
+        repositionRight();
+    }
+    public void harvestOneRow() {
+        pickBeeper();
+        move();
+        pickBeeper();
+        move();
+        pickBeeper();
+        move();
+        pickBeeper();
+        move();
+        pickBeeper();
+    }
+    public void repositionRight()  {
+        turnRight();
+        move();
+        turnRight();
+    }
+    public void repositionLeft()  {
+        turnLeft();
+        move();
+        turnLeft();       
+    }
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    public void sowTwoRows() {
+        sowOneRow();
+        repositionLeft();
+        sowOneRow();
+        repositionRight();
+    }
+    public void repositionOrigin() {
+        turnLeft();
+        turnLeft();
+        move();
+        turnLeft();
+        move();
+        move();
+        move();
+        move();
+        move();
+        move();
+        turnLeft();
+        move();
+    }
+    public void sowOneRow() {
+        putBeeper();
+        move();
+        putBeeper();
+        move();
+        putBeeper();
+        move();
+        putBeeper();
+        move();
+        putBeeper();
+    }
 }
 
