515c2562e982425b5fe233416f05eb16bc157cbd
EdCollinson1502 <31353239+EdCollinson1502@users.noreply.github.com>
2017-09-01 08:59:52 -0700

    Day 2

--- Lab 1/lab_2/individual_lab/Problem2.java
+++ Lab 1/lab_2/individual_lab/Problem2.java
@@ -15,6 +15,153 @@ public class Problem2 extends Robot
     }
 
     public void setField(){
+        setRowOne();
+        setRowTwo();
+        setRowThree();
+        setRowFour();
+        setRowFive();
+        setRowSix();
+        setRowSeven();
+    }
+    public void setRowOne(){
+        move();
+        putBeeper();
+        turnRight();
+        move();
+        turnLeft();
+        move();
+    }
+    public void setRowTwo(){
+        putBeeper();
+        turnLeft();
+        moveTwo();
+        putBeeper();
+        move();
+        turnRight();
+        move();
+        turnRight();
+    }
+    public void setRowThree(){
+        putBeeper();
+        moveTwo();
+        putBeeper();
+        moveTwo();
+        putBeeper();
+        move();
+        turnLeft();
+        move();
+        turnLeft();
+    }
+    public void setRowFour(){
+        putBeeper();
+        moveTwo();
+        putBeeper();
+        moveTwo();
+        putBeeper();
+        moveTwo();
+        putBeeper();
+        move();
+        turnRight();
+        move();
+        turnRight();
+    }
+      public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        
+    }
+    public void pickAndRun() {
+        pickBeeper();
+        turnLeft();
+        move();
+        move();
+        move();
+    }
+    public void finalDump() {
+            putBeeper();
+            putBeeper();
+            putBeeper();
+            putBeeper();
+        
+        
+    }
+    public void miniRun() {
+            pickBeeper();
+            move();
+            move();
+            move();
+        
+    }
+    public void moveThree() {
+        move();
+        move();
+        move();
+    }
+    public void moveTwo() {
+        move();
+        move();
+        
+    }
+    public void doubleRight() {
+        turnRight();
+            turnRight();
+        
+    }
+    public void doublePAR() {
+        pickAndRun();
+            pickAndRun();
+        
+    }
+    public void pickThenMove()  {
+        pickBeeper();
+        move();
+        pickBeeper();
+        move();
+        pickBeeper();
+        move();
+        pickBeeper();
+        move();
+        pickBeeper();
+    }
+    public void turn180() {
+        turnLeft();
+        turnLeft();
+    }
+    public void MTOL() {
+        
+        turnLeft();
+        moveTwo();
+        turnLeft();
+      
+    }
+    public void MTAR() {
+        turnRight();
+        moveTwo();
+        turnRight();
+        moveTwo();
+        move();
+        turnRight();
+        
+    }
+    public void placeAllFive() {
+        putBeeper();
+        move();
+        putBeeper();
+        move();
+        putBeeper();
+        move();
+        putBeeper();
+        move();
+        putBeeper();
+        move();
+        
+        
+    }
+    public void MTM() {
+        moveTwo();
+        turnRight();
+        moveTwo();
         
     }
 }
