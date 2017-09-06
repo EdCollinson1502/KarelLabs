d99f38c81f8dbbee783ce77a5438173d458013e2
EdCollinson1502 <31353239+EdCollinson1502@users.noreply.github.com>
2017-09-06 09:21:59 -0700

    Computer # 5 Initial Setup

---
+++ Unit 1/lab_3/individual_lab/Problem1Driver.java
@@ -0,0 +1,35 @@
+package lab_3.individual_lab;
+
+
+/**
+ * Write a description of class Problem1Driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Problem1Driver
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Problem1Driver
+     */
+    public Problem1Driver()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
