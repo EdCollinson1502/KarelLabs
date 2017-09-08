2b64ce4e5ba303810b05183483aeaf2c62329743
EdCollinson1502 <31353239+EdCollinson1502@users.noreply.github.com>
2017-09-08 09:43:44 -0700

    8th of Sep

--- Unit 1/lab_3/individual_lab/Problem2.java
+++ Unit 1/lab_3/individual_lab/Problem2.java
@@ -18,16 +18,157 @@ public class Problem2 extends Robot
         MoveToRoom();
         CarpetRoom();
         LeaveRoom();
+        MoveToRoom();
+        CarpetRoom();
+        LeaveRoom();
+        MoveToRoom();
+        CarpetRoom();
+        LeaveRoom();
+        MoveToRoom();
+        CarpetRoom();
+        LeaveRoom();
+        MoveToRoom();
+        CarpetRoom();
+        LeaveRoom();
+        MoveToRoom();
+        CarpetRoom();
+        LeaveRoom();
+        MoveToRoom();
+        CarpetRoom();
+        LeaveRoom();
+        MoveToRoom();
+        CarpetRoom();
+        LeaveRoom();
     }
     public void MoveToRoom(){
-        
+        move();
+        turnLeft();
+        move();        
     }
+
     public void CarpetRoom(){
-       
-       
+        if(checkLeftWall() && checkRightWall() && checkCeiling()){
+            putBeeper();
+        }
     }
     public void LeaveRoom(){
+        faceSouth();
+        move();
+        turnLeft();
+    }
+    public boolean checkLeftWall(){
+        faceWest();
+        if(frontIsClear()) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public boolean checkRightWall(){
+        faceEast();
+        if(frontIsClear()) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public boolean checkCeiling(){
+        faceNorth();
+        if(frontIsClear()) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    public void moveThree() {
+        move();
+        move();
+        move();
+    }
+    public void moveTwo() {
+        move();
+        move();
         
     }
+    public void turn180() {
+        turnLeft();
+        turnLeft();
+    }
+    public void faceEast(){
+        if(facingWest()) {
+                turnLeft();
+                turnLeft();
+        }
+        else
+        {
+                if(facingNorth())  { 
+                        turnRight();
+                }
+                else {
+                    if(facingWest()) {
+                        turnLeft();
+                    }    
+                }
+        }
+    }         
+    public void faceNorth(){
+        if(facingSouth()) {
+                turnLeft();
+                turnLeft();
+        }
+        else
+        {
+                if(facingWest())  { 
+                        turnRight();
+                }
+                else {
+                    if(facingEast()) {
+                        turnLeft();
+                    }    
+                }
+        }
+    }         
+    public void faceSouth(){
+        if(facingNorth()) {
+                turnLeft();
+                turnLeft();
+        }
+        else
+        {
+                if(facingEast())  { 
+                        turnRight();
+                }
+                else {
+                    if(facingWest()) {
+                        turnLeft();
+                    }    
+                }
+        }
+    }         
+    public void faceWest(){
+        if(facingEast()) {
+                turnLeft();
+                turnLeft();
+        }
+        else
+        {
+                if(facingSouth())  { 
+                        turnRight();
+                }
+                else {
+                    if(facingNorth()) {
+                        turnLeft();
+                    }    
+                }
+        }
+    }         
 }
 
